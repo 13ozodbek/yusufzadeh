035d88a11285ca4772e2d1fceb8897038abf86cc
ozodbek <ozodbekkkk13@icloud.com>
2024-08-20 23:53:02 +0500

    .

--- home/utils.py
+++ home/utils.py
@@ -1,8 +1,9 @@
 import requests
-from django.utils import timezone
+
 CHAT_ID = '584066666'
 TELEGRAMBOT_URL = "https://api.telegram.org/bot{}/sendMessage?text={}&chat_id={}"
 BOT_ID = "7543378625:AAEOaVAQRyBItEtUFZ8198DxovyS_eEnEeU"
+
 
 def send_message(message):
     message = (f'Name: {message.name}\n'
@@ -10,7 +11,9 @@ def send_message(message):
                f'phone: {message.phone}\n'
                '\n'
                f'message: {message.message}'
+               '\n'
+               f'date: {message.created_at}'
 
-    )
+               )
     response = requests.get(TELEGRAMBOT_URL.format(BOT_ID, message, CHAT_ID))
     return response
